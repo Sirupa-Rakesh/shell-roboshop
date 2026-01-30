cba9bd11940c5d9d9b5e054e8db201ae77c76055
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-01-30 21:42:39 +0530

    actions

---
+++ roboshop.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+SG_ID="sg-08f36e4129a352630" #replace with your id
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do 
+     instance_id=$( aws ec2 run-instance \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \    
+    --tagSpecifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query "Instances[0].InstanceId" \
+    --output text \ )
+
+    if [  $instance == "frontend" ] then 
+        IP=$(
+             aws ec2 describe-instances \
+            --instance-ids $instance_id \
+            --query "Reservations[].Instances[].PublicIpAddress" \
+            --output text \ 
+        )
+        else
+         IP=$(
+             aws ec2 describe-instances \
+            --instance-ids $instance_id \
+            --query "Reservations[].Instances[].PrivateIpAddress" \
+            --output text \ 
+        )
+    fi
+done
